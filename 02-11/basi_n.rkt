1e0e4891a21da9d250835bd0b023683213c25357
Jacopo <93609238+jagaaa1@users.noreply.github.com>
2021-11-03 13:54:56 +0100

    Add files via upload
    codice commentato

--- 02-11/basi_n.rkt
+++ 02-11/basi_n.rkt
@@ -3,8 +3,8 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname basi_n) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
 (require racket/string)
-(define stringa-val
-    (lambda (val base)
+(define stringa-val ; data una cifra (formato stringa) e una stringa restituisce una sottonstringa che parte dal valore dato
+    (lambda (val base) ; val: stringa formata da una cifra, base: stringa formata da cifre ordinate di una base numerica
         (if (string=? (substring base 0 1) val)
             base
             (stringa-val val (substring base 1))
@@ -12,9 +12,9 @@
     )
 )
 
-(define valore
-    (lambda (val base)
-        (-
+(define valore ; data una cifra (formato stringa) e una stringa rappresentante una base restituisce il valore in base 10 della cifra nella base fornita
+    (lambda (val base) ; val: stringa formata da una cifra, base: stringa formata da cifre ordinate di una base numerica
+        (- ; il valore si ottiene sottraendo la lunghezza della sottostringa descritta sopra alla lunghezza della stringa base
             (string-length base)
             (string-length
                 (stringa-val val base)
@@ -23,8 +23,8 @@
     )
 )
 
-(define b-dec
-    (lambda (base str)
+(define b-dec ; converte un numero intero da una base fornita a base 10
+    (lambda (base str) ; base: stringa formata da cifre ordinate di una base numerica, str: numero intero (formato stringa)
         (let ((k (- (string-length str) 1)))
             (if (= k 0)
                 (valore str base)
@@ -37,7 +37,7 @@
     )
 )
 
-(define parte_intera
+(define parte_intera ; restituisce la parte prima della virgola ( . ) di un numero (formato stringa) togliendo anche eventuale segno (+ o -)
     (lambda (str)
         (if (string=? (substring str (- (string-length str) 1))".")
             (if (or (string=? (substring str 0 1) "+") (string=? (substring str 0 1) "-"))
@@ -49,7 +49,7 @@
     )
 )
 
-(define parte_fraz
+(define parte_fraz ; restituisce la parte dopo la virgola ( . ) di un numero (formato stringa)
     (lambda (str)
         (if (string=? (substring str 0 1)".")
             (substring str 1)
@@ -58,13 +58,15 @@
     )
 )
 
-(define rep->number ; conversione stringa formata da 0, 1, +, -, . in numero decimale
-    (lambda (base str)
-        (if (string-contains? str ".") ; = se la stringa str contiene "."
-; per convertire un numero bin con virgola si può anche convertire il numero ottenuto ignorando la virgola 
-; e dividendolo successivamente per una potenza di 2 con all'esponente il numero di cifre dopo la virgola 
-; es. 10.1 bin = 2.5 dec --> 10.1 bin = 101 bin / 2^1 = 5/2 = 2.5 dec
-            (cond ; (exact->inexact) trasforma una frazione in numero con virgola es. 5/2 -> 2.5
+(define rep->number ; dato un numero (formato stringa) e una base converte in base 10
+    (lambda (base str) ; base: stringa formata da cifre ordinate di una base numerica, str: numero formato da cifre contenute in base,
+                        ; con eventuali segno (+ o -) e virgola ( . )
+        (if (string-contains? str ".")
+                    ; se un numero da convertire contiene la virgola si può procedere convertendo invece
+                    ; il numero ottenuto ignorando la virgola e dividendo poi il risultato per una potenza 
+                    ; della base di partenza (es. base 2 -> 2, base H -> 16) con esponente il numero di cifre dopo la virgola
+                    ; es. 10.1 in base 2 --> 101 base 2 / 2^1 = 5/2 = 2.5
+            (cond
                 ((string=? (substring str 0 1) "+")
                     (exact->inexact 
                         (/ 
@@ -90,7 +92,7 @@
                 
                 )
             )
-            (cond ; se nella stringa passata non è presente la virgola si converte normalmente (togliendo solo il + o -)
+            (cond ; se il numero fornito non ha la virgola si converte normalmente, togliendo prima eventuale segno (+ o -)
                 ((string=? (substring str 0 1) "+") (b-dec base (substring str 1)))
                 ((string=? (substring str 0 1) "-") (* (b-dec base (substring str 1)) -1))
                 (else (b-dec base str))
